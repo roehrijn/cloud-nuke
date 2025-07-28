1eba2413caf87206756db7e6436d771549f2f02d
Jan Roehrich <jan.roehrich@mercedes-benz.com>
2025-07-28 09:10:26 +0200

    feat(config): Re-implement setting of external AWS credentials

--- externalcreds/creds.go
+++ externalcreds/creds.go
@@ -2,19 +2,36 @@ package externalcreds
 
 import (
 	"context"
+	"sync"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 )
 
-func Get(region string) (aws.Config, error) {
-	cfg, err := config.LoadDefaultConfig(context.TODO(),
-		config.WithRegion(region),
-	)
+var configsByRegion = map[string]aws.Config{}
+var lock sync.Mutex
 
-	if err != nil {
-		return aws.Config{}, err
+func Get(region string) (aws.Config, error) {
+	lock.Lock()
+	defer lock.Unlock()
+
+	cfg, found := configsByRegion[region]
+	if !found {
+		var err error
+		cfg, err = config.LoadDefaultConfig(context.TODO(),
+			config.WithRegion(region),
+		)
+		if err != nil {
+			return aws.Config{}, err
+		}
+		configsByRegion[region] = cfg
 	}
 
 	return cfg, nil
+}
+
+func Set(region string, cfg aws.Config) {
+	lock.Lock()
+	defer lock.Unlock()
+	configsByRegion[region] = cfg
 }
